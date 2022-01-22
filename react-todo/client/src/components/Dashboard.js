4603772e476318ec5960e03a9c03f53bbcf6bb7b
Pavlos Saba <pavlossaba@gmail.com>
2022-01-22 19:51:34 +0200

    Finished application with auth,create,update,save,delete todo

--- react-todo/client/src/components/Dashboard.js
+++ react-todo/client/src/components/Dashboard.js
@@ -1,17 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../context/GlobalContext";
+import NewToDo from "./NewToDo";
+import TodoCard from "./TodoCard";
 
 const Dashboard = () => {
-    const { user } = useGlobalContext();
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (!user && navigate) {
-          navigate("/");
-        }
-      }, [user, navigate]);
-    
-  return <h2>Dashboard</h2>;
+  const { user, completeToDos, incompleteToDos } = useGlobalContext();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user && navigate) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
+  return (
+    <div className="dashboard">
+        <NewToDo />
+      <div className="todos">
+        <h1 className="todos__title">Incomplete Todos</h1>
+        {incompleteToDos.map((toDo) => (
+          <TodoCard toDo={toDo} key={toDo._id} />
+          
+        ))}
+      </div>
+
+      {completeToDos.length > 0 && (
+        <div className="todos">
+          <h1 className="todos__title">Complete Todos</h1>
+          {completeToDos.map((toDo) => (
+            <TodoCard toDo={toDo} key={toDo._id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
--- react-todo/client/src/context/GlobalContext.js
+++ react-todo/client/src/context/GlobalContext.js
@@ -17,24 +17,24 @@ const globalReducer = (state, action) => {
         user: action.payload,
         fetchingUser: false,
       };
-      case "SET_COMPLETE_TODOS":
+    case "SET_COMPLETE_TODOS":
       return {
         ...state,
         completeToDos: action.payload,
       };
-      case "SET_INCOMPLETE_TODOS":
+    case "SET_INCOMPLETE_TODOS":
       return {
         ...state,
         incompleteToDos: action.payload,
       };
-      case "RESET_USER": 
+    case "RESET_USER":
       return {
-          ...state,
+        ...state,
         user: null,
         fetchingUser: false,
         completeToDos: [],
         incompleteToDos: [],
-      }
+      };
     default:
       return state;
   }
@@ -49,7 +49,7 @@ export const GlobalProvider = (props) => {
   useEffect(() => {
     getCurrentUser();
   }, []);
-    
+
   //action: get current user
   const getCurrentUser = async () => {
     try {
@@ -67,30 +67,105 @@ export const GlobalProvider = (props) => {
             payload: toDosRes.data.incomplete,
           });
         }
-      }else {
-          dispatch( {type: "RESET_USER"})
+      } else {
+        dispatch({ type: "RESET_USER" });
       }
     } catch (error) {
       console.log(error);
-      dispatch( {type: "RESET_USER"})
-
+      dispatch({ type: "RESET_USER" });
     }
   };
 
-const logout = async () => {
+  const logout = async () => {
     try {
-        await axios.put("/api/auth/logout");
-        dispatch({type: "RESET_USER"})
+      await axios.put("/api/auth/logout");
+      dispatch({ type: "RESET_USER" });
     } catch (error) {
-        console.log(error);
-        dispatch({type: "RESET_USER"})
+      console.log(error);
+      dispatch({ type: "RESET_USER" });
     }
-}
+  };
 
+  const addToDo = (toDo) => {
+    dispatch({
+      type: "SET_INCOMPLETE_TODOS",
+      payload: [toDo, ...state.incompleteToDos],
+    });
+  };
+
+  const toDoComplete = (toDo) => {
+    dispatch({
+      type: "SET_INCOMPLETE_TODOS",
+      payload: state.incompleteToDos.filter(
+        (incompleteTodo) => incompleteTodo._id !== toDo._id
+      ),
+    });
+    dispatch({
+      type: "SET_COMPLETE_TODOS",
+      payload: [toDo, ...state.completeToDos],
+    });
+  };
+  const toDoIncomplete = (toDo) => {
+    dispatch({
+      type: "SET_COMPLETE_TODOS",
+      payload: state.completeToDos.filter(
+        (completeTodo) => completeTodo._id !== toDo._id
+      ),
+    });
+    const newIncompleteToDos = [toDo, ...state.incompleteToDos];
+    dispatch({
+      type: "SET_INCOMPLETE_TODOS",
+      payload: newIncompleteToDos.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      ),
+    });
+  };
+  const removeToDo = (toDo) => {
+    if (toDo.complete) {
+      dispatch({
+        type: "SET_COMPLETE_TODOS",
+        payload: state.completeToDos.filter(
+          (completeTodo) => completeTodo._id !== toDo._id
+        ),
+      });
+    } else {
+      dispatch({
+        type: "SET_INCOMPLETE_TODOS",
+        payload: state.incompleteToDos.filter(
+          (incompleteTodo) => incompleteTodo._id !== toDo._id
+        ),
+      });
+    }
+  };
+
+  const updateToDo = (toDo) => {
+    if (toDo.complete) {
+      const newCompleteToDos = state.completeToDos.map((completeTodo) =>
+        completeTodo._id !== toDo._id ? completeTodo : toDo
+      );
+      dispatch({
+        type: "SET_COMPLETE_TODOS",
+        payload: newCompleteToDos,
+      });
+    } else {
+      const newIncompleteToDos = state.incompleteToDos.map((incompleteTodo) =>
+        incompleteTodo._id !== toDo._id ? incompleteTodo : toDo
+      );
+      dispatch({
+        type: "SET_INCOMPLETE_TODOS",
+        payload: newIncompleteToDos,
+      });
+    }
+  };
   const value = {
     ...state,
     getCurrentUser,
     logout,
+    addToDo,
+    toDoComplete,
+    toDoIncomplete,
+    removeToDo,
+    updateToDo,
   };
   return (
     <GlobalContext.Provider value={value}>
